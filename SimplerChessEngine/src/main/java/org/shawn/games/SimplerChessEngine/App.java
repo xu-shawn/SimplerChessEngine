babdba5ccdc7c5b4b4eee39339a0aa1a046dc3c7
Shawn Xu <xu107288696@gmail.com>
2024-01-09 11:39:51 -0800

    Initial Import

---
+++ SimplerChessEngine/src/main/java/org/shawn/games/SimplerChessEngine/App.java
@@ -0,0 +1,13 @@
+package org.shawn.games.SimplerChessEngine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
