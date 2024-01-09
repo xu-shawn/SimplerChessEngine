e70f7d225e23f20651a0eb4e96aafaf4fb933108
Shawn Xu <xu107288696@gmail.com>
2024-01-09 13:41:13 -0800

    Updated demo

--- SimplerChessEngine/src/main/java/org/shawn/games/SimplerChessEngine/TUI.java
+++ SimplerChessEngine/src/main/java/org/shawn/games/SimplerChessEngine/TUI.java
@@ -7,6 +7,13 @@ public class TUI
 	public static void main(String args[])
 	{
 		MatchMaker match = new MatchMaker(new RandomMover(), new RandomMover());
+		int[] results = match.play(10000);
+		System.out.print("WDL: ");
+		System.out.print(results[0]);
+		System.out.print("-");
+		System.out.print(results[2]);
+		System.out.print("-");
+		System.out.println(results[1]);
 		System.out.println(match.demonstrationMatch(false));
 	}
 }
