dbbd3f22051899763df24e549c8cc97982052a3e
Shawn Xu <xu107288696@gmail.com>
2024-04-09 21:22:53 -0700

    Revert "another demo"
    This reverts commit cec06fcee1fa3d8e5b192a6b7f44c6416b91c137.

--- SimplerChessEngine/src/main/java/org/shawn/games/SimplerChessEngine/Algorithms/SimpleAlphaBeta.java
+++ SimplerChessEngine/src/main/java/org/shawn/games/SimplerChessEngine/Algorithms/SimpleAlphaBeta.java
@@ -1,7 +1,5 @@
 package org.shawn.games.SimplerChessEngine.Algorithms;
 
-import java.util.List;
-
 import com.github.bhlangonijr.chesslib.*;
 import com.github.bhlangonijr.chesslib.move.*;
 
@@ -10,60 +8,10 @@ public class SimpleAlphaBeta
 	private static Move bestMove;
 	private static long nodes;
 
-	private static int qsearch(int alpha, int beta, Board board, int ply)
-	{
-		nodes++;
-
-		if (board.isMated())
-		{
-			return -100000;
-		}
-
-		if (board.isDraw())
-		{
-			return 0;
-		}
-
-		if (!board.isKingAttacked())
-		{
-			int standPat = PeSTO.evaluate(board);
-			alpha = Math.max(alpha, standPat);
-
-			if (alpha >= beta)
-			{
-				return beta;
-			}
-		}
-
-		final List<Move> moves = board.isKingAttacked() ? board.pseudoLegalMoves() : board.pseudoLegalCaptures();
-
-		for (Move move : moves)
-		{
-			if (!board.isMoveLegal(move, true))
-			{
-				continue;
-			}
-
-			board.doMove(move);
-
-			nodes++;
-			int score = -qsearch(-beta, -alpha, board, ply + 1);
-
-			board.undoMove();
-
-			alpha = Math.max(alpha, score);
-
-			if (alpha >= beta)
-			{
-				return beta;
-			}
-		}
-
-		return alpha;
-	}
-
 	public static int search(int depth, int alpha, int beta, Board board, int ply)
 	{
+		nodes ++;
+		
 		if (board.isMated())
 		{
 			return -100000;
@@ -76,7 +24,7 @@ public class SimpleAlphaBeta
 
 		if (depth <= 0)
 		{
-			return qsearch(alpha, beta, board, ply);
+			return PeSTO.evaluate(board);
 		}
 
 		int bestValue = Integer.MIN_VALUE;
@@ -85,7 +33,6 @@ public class SimpleAlphaBeta
 		{
 			board.doMove(move);
 
-			nodes++;
 			int thisMoveValue = -search(depth - 1, -beta, -alpha, board, ply + 1);
 
 			board.undoMove();
